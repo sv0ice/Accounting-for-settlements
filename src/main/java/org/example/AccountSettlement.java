27db9bba9effe83622f92b3a3ddf31ce97d344e5
sv0ice <sakurav0iceee@gmail.com>
2023-04-20 00:27:43 +0600

    Add method createExcel()

--- src/main/java/org/example/AccountSettlement.java
+++ src/main/java/org/example/AccountSettlement.java
@@ -19,9 +19,9 @@ public class AccountSettlement {
 //    }
 
     private XSSFSheet sheet;
-    private String nameSupp;
+    //    private String nameSupp;
     private HashMap<Character, Integer> columns = new HashMap<>();
-    private final HashMap<String, Double> states = new HashMap<>();
+    private HashMap<String, Double> states = new HashMap<>();
     private ArrayList<String> arrList = new ArrayList<>();
 
     // Add method that accepts Excel file as parameter | String readExcel()
@@ -39,28 +39,27 @@ public class AccountSettlement {
     public void getNameSupp() {
         System.out.println("Enter name of the supplier: ");
         Scanner sc = new Scanner(System.in);
-        this.nameSupp = sc.nextLine();
+//        this.nameSupp = sc.nextLine();
     }
 
     // I can add 1-sheet, 2-columns | parameter
     public void getData() {
-        getNameSupp();
+//        getNameSupp();
         for (Row r : sheet) {
-
             // Add method that accepts letter as number of column | char getColumn()
             Cell cStr = r.getCell(3);
             Cell cInt = r.getCell(4);
             Cell cData = r.getCell(5);
             Cell cStatus = r.getCell(2);
 
-            if (cStr != null & cInt != null & cStr.getStringCellValue().contains(nameSupp) & cStatus.getStringCellValue().contains("Принят от поставщика")) {
+            if (cStr != null & cInt != null & cStatus.getStringCellValue().contains("Принят от поставщика")) {
                 if (cStr.getCellType() == CellType.STRING & cInt.getCellType() == CellType.NUMERIC) {
                     if (states.containsKey(cStr.getStringCellValue())) {
-                        System.out.println(cStr.getStringCellValue() + " " + cInt.getNumericCellValue() + " | " + cData.getStringCellValue());
+//                        System.out.println(cStr.getStringCellValue() + " " + cInt.getNumericCellValue() + " | " + cData.getStringCellValue());
                         arrList.add(cStr.getStringCellValue());
                         states.put(cStr.getStringCellValue(), states.get(cStr.getStringCellValue()) + cInt.getNumericCellValue());
                     } else {
-                        System.out.println(cStr.getStringCellValue() + " " + cInt.getNumericCellValue() + " | " + cData.getStringCellValue());
+//                        System.out.println(cStr.getStringCellValue() + " " + cInt.getNumericCellValue() + " | " + cData.getStringCellValue());
                         states.put(cStr.getStringCellValue(), cInt.getNumericCellValue());
                     }
                 } else if (cStr.getCellType() == CellType.FORMULA && cStr.getCachedFormulaResultType() == CellType.NUMERIC && cInt.getCellType() == CellType.FORMULA && cInt.getCachedFormulaResultType() == CellType.NUMERIC) {
@@ -74,49 +73,52 @@ public class AccountSettlement {
         int size = 1;
 
         for (String elem : arrList) {
-//                elem.contains("KENDALA IMPEX TOO") ? size++;
-            if (elem.contains(nameSupp)) {
-                size++;
-            }
+//            elem.contains("KENDALA IMPEX TOO") ? size++;
+            size++;
+//            if (elem.contains(nameSupp)) {
+//                size++;
+//            }
         }
         System.out.println("=========================================\n");
         System.out.println("Suppliers found by name: " + size);
-        System.out.println("Sum: " + states.get(nameSupp));
+        System.out.println("Sum: " + states);
     }
 
-    public void run() {
+    public void createExcel() {
+        try {
 
+            Set<String> keys = states.keySet();
+            Collection<Double> values = states.values();
+
+//             Создание нового Excel-документа
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet("Sheet 1");
+            Row headerRow = sheet.createRow(0);
+
+            headerRow.createCell(0).setCellValue("Контрагент");
+            headerRow.createCell(1).setCellValue("Сумма");
+
+            ArrayList<String> keyList = new ArrayList<>();
+            ArrayList<Double> valuesList = new ArrayList<>();
+            for (int i = 1; i <= keys.size(); i++) {
+                keyList.add((String) keys.toArray()[i - 1]);
+                valuesList.add((Double) values.toArray()[i - 1]);
+                Row row = sheet.createRow(i);
+
+                row.createCell(0).setCellValue(keyList.get(i - 1));
+                row.createCell(1).setCellValue(valuesList.get(i - 1));
+            }
+            sheet.autoSizeColumn(0);
+            sheet.autoSizeColumn(1);
+//             Запись данных в файл
+            FileOutputStream outputStream = new FileOutputStream("Результат.xlsx");
+            workbook.write(outputStream);
+            workbook.close();
+            System.out.println("Результат записан в Excel-документ");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
-    public void setColumn() {
-
-    }
-
-    public void getColumn(HashMap<Character, Integer> columns, HashMap<String, Double> states) {
-
-    }
-
-
-//    try {
-//        Set keys =
-//                // Создание нового Excel-документа
-//                // XSSFWorkbook workbook = new XSSFWorkbook();
-//        XSSFSheet sheet = workbook.createSheet("Sheet 1");
-//        // Создание формулы в ячейке B1
-//        XSSFRow row = sheet.createRow(0);
-//        XSSFCell cellA1 = row.createCell(0);
-//        XSSFCell cellB1 = row.createCell(1);
-//        cellA1.setCellValue("Контрагент");
-//        cellB1.setCellValue("Сумма документа");
-//        cellB1.setCellFormula("A1*2");
-//        // Запись данных в файл
-//        FileOutputStream outputStream = new FileOutputStream("Результат.xlsx");
-//        workbook.write(outputStream);
-//        workbook.close();
-//        System.out.println("Результат записан в Excel-документ");
-//    } catch (Exception e) {
-//        e.printStackTrace();
-//    }
 
 }
 
